dc8aa09c3cb8ed87c47a338a6f4d0b6cc0934c21
Adam Szmajdziński <a.szmajdzinski@gmail.com>
2022-02-19 15:09:49 +0100

    Added testcase for steering (servo).

--- raspberry/tests/Hardware/test_motors.py
+++ raspberry/tests/Hardware/test_motors.py
@@ -26,26 +26,47 @@ def expected_motor_value(value, config):
         return int(interp(value, [0, 128], [config.motor_minimum_power, config.motor_maximum_power]))
 
 
+def expected_steer_value(value, config):
+    angle = 0
+    if value == 0:
+        angle = config.steering_servo_range[1]
+    elif value < 0:
+        angle = interp(value,
+                       [-128, 0],
+                       [config.steering_servo_range[0], config.steering_servo_range[1]])
+
+    elif value > 0:
+        angle = interp(value,
+                       [0, 128],
+                       [config.steering_servo_range[1], config.steering_servo_range[2]])
+    return int(angle)
+
+
 @pytest.fixture(params=configs)
 def get_config(request):
     yield request.param
 
 
 @pytest.fixture(params=[-127, -50, 50, 128])
-def get_input_value(request):
+def get_accelerate_input_value(request):
     yield request.param
 
 
-def test_accelerate(get_config, get_input_value, mocker):
+@pytest.fixture(params=[-128, -50, 0, 50, 128])
+def get_steering_input_value(request):
+    yield request.param
+
+
+def test_accelerate(get_config, get_accelerate_input_value, mocker):
     arduino = mocker.patch("raspberry.Hardware.arduinoserial.ArduinoCommunication.send_command")
     info_queue = Queue()
 
-    expected_value = expected_motor_value(get_input_value, get_config)
+    expected_value = expected_motor_value(get_accelerate_input_value, get_config)
     expected_arduino_command = NameValueTuple(name=ArduinoCommandList.MOTOR, value=expected_value)
     expected_info_queue_element = NameValueTuple(name=InfoList.MOTOR_VALUE_UPDATED, value=expected_value)
 
     motors = MotorControl(arduino=arduino, config=get_config, info_queue=info_queue)
-    motors.accelerate(get_input_value)
+    motors.accelerate(get_accelerate_input_value)
 
     arduino.send_command.assert_called_once_with(expected_arduino_command)
     assert expected_info_queue_element == info_queue.get()
@@ -61,6 +82,21 @@ def test_stop(get_config, mocker):
 
     motors = MotorControl(arduino=arduino, config=get_config, info_queue=info_queue)
     motors.accelerate(0)
+
+    arduino.send_command.assert_called_once_with(expected_arduino_command)
+    assert expected_info_queue_element == info_queue.get()
+
+
+def test_steering(get_config, get_steering_input_value, mocker):
+    arduino = mocker.patch("raspberry.Hardware.arduinoserial.ArduinoCommunication.send_command")
+    info_queue = Queue()
+
+    expected_value = expected_steer_value(get_steering_input_value, get_config)
+    expected_arduino_command = NameValueTuple(name=ArduinoCommandList.STEERING_SERVO, value=expected_value)
+    expected_info_queue_element = NameValueTuple(name=InfoList.STEERING_VALUE_UPDATED, value=expected_value)
+
+    motors = MotorControl(arduino=arduino, config=get_config, info_queue=info_queue)
+    motors.steer(get_steering_input_value)
 
     arduino.send_command.assert_called_once_with(expected_arduino_command)
     assert expected_info_queue_element == info_queue.get()
